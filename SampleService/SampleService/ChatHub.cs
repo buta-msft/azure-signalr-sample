077b511ba2bb9820c9e672a9323b6877351cb993
Buu-Minh Ta <buta@microsoft.com>
2021-01-06 12:19:08 -0800

    Sample service

---
+++ SampleService/SampleService/ChatHub.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SampleService
+{
+    public class ChatHub : Hub
+    {
+        public Task BroadcastMessage(string name, string message)
+        {
+            return this.Clients.All.SendAsync("broadcastMessage", name, message);
+        }
+
+        public Task Echo(string name, string message) =>
+            this.Clients.Client(this.Context.ConnectionId)
+                .SendAsync("echo", name, $"{message} (echo from server)");
+    }
+}
